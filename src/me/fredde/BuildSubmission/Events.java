7049755f8441bca65a56347739368b838b281ac3
BuildTools <andersson.fredde@icloud.com>
2017-01-19 20:41:56 +0100

    New draft of feature "Rank permission".

--- src/me/fredde/BuildSubmission/Events.java
+++ src/me/fredde/BuildSubmission/Events.java
@@ -4,6 +4,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.permissions.PermissionAttachmentInfo;
 
 import java.util.List;
 import java.util.UUID;
@@ -23,55 +24,17 @@ class Events implements Listener {
         UUID uuid = player.getUniqueId();
         boolean isNew = !builders.stream().filter(p -> p.getUuid().equals(uuid)).findFirst().isPresent();
 
-        if (isNew) {
-            if (!hasRankPermission(player)) builders.add(new Builder(settings.BUILDERS, uuid, 0, null));
-        }
+        if (isNew) addBuilder(player);
     }
 
-    private boolean hasRankPermission(Player player) {
-        boolean state = false;
-        int highestRank = 0;
+    private void addBuilder(Player player) {
+        int rank = 0;
 
-        // TODO: Combine the command loop below.
-
-        for (Object o : settings.ONLINE) {
-            String cmd = o.toString();
-            cmd = cmd.trim();
-
-            int i = cmd.indexOf("}");
-
-            if (i > 0) {
-                // TODO: Assuming that it's always index 5 which could be fetched dynamically instead.
-                String rank = cmd.substring(5, i - 1);
-                highestRank = Integer.valueOf(rank);
-            }
+        for (PermissionAttachmentInfo pai : player.getEffectivePermissions()) {
+            String permission = pai.getPermission();
+            if (permission.startsWith("bs.rank")) rank = Integer.valueOf(permission.substring(6));
         }
 
-        for (Object o : settings.OFFLINE) {
-            String cmd = o.toString();
-            cmd = cmd.trim();
-
-            int i = cmd.indexOf("}");
-
-            if (i > 0) {
-                // TODO: Assuming that it's always index 5 which could be fetched dynamically instead.
-                String rank = cmd.substring(5, i - 1);
-                int temp = Integer.valueOf(rank);
-                if (temp > highestRank) highestRank = temp;
-            }
-        }
-
-        Integer rank = null;
-
-        for (int i = 0; i <= highestRank; i++) {
-            if (player.hasPermission("bs.rank" + i)) rank = i;
-        }
-
-        if (rank != null) {
-            builders.add(new Builder(settings.BUILDERS, player.getUniqueId(), rank, null));
-            state = true;
-        }
-
-        return state;
+        builders.add(new Builder(settings.BUILDERS, player.getUniqueId(), rank, null));
     }
 }
